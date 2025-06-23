9fe9a2a0ccadd92f6c3f8c25bd9152279c456e6b
Marcos Mendez <marcosmendezser1@gmail.com>
2025-06-23 16:34:24 +0200

    [Docker] feature: adding MongoDB

--- app/server.ts
+++ app/server.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response } from "express";
 import dotenv from "dotenv";
+import mongoose from 'mongoose';
 
 // configures dotenv to work in your application
 dotenv.config();
@@ -10,6 +11,18 @@ const PORT = process.env.PORT;
 app.get("/", (request: Request, response: Response) => { 
   response.status(200).send("Hello World");
 }); 
+
+const mongoUri = process.env.MONGO_URI || 'mongodb://localhost:27017/mydatabase';
+
+mongoose.connect(mongoUri)
+  .then(() => {
+    console.log('Connected to MongoDB');
+    // Start your server here if you want to wait for DB connection
+  })
+  .catch(err => {
+    console.error('Failed to connect to MongoDB', err);
+  });
+
 
 app.listen(PORT, () => { 
   console.log("Server running at PORT: ", PORT); 
